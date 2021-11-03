d7711c39013987df913740a489c75ad8127b6ebd
nagyb9 <nagyb95@gmail.com>
2021-11-03 22:38:59 +0100

    try-except

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
 import requests
 def get_random_dad_joke():
     url = "https://icanhazdadjoke.com/"
-    answer = requests.get(url, timeout=3, headers={'Accept':'application/json'}).json()
-    print(answer)
     try:
-        return answer['joke']
+        answer = requests.get(url, timeout=3, headers={'Accept': 'application/json'}).json()
     except:
         return answer['status']
+    return answer['joke']
