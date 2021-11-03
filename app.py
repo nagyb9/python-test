182adf68f0841cd0ba31976bda60dc86520b80a3
nagyb9 <nagyb95@gmail.com>
2021-11-03 20:12:10 +0100

    task-03

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, make_response, render_template, Response
 
+import utils
+from utils import *
+
 app = Flask(__name__)
 
 @app.route("/")
 def default():
     return make_response(render_template(r"index.html"))
 
-@app.route("/test", methods=["GET"])
+@app.route("/test1", methods=["GET"])
 def test():
     return Response(str('python-test works!'), mimetype="text/plain")
+
+@app.route("/random")
+def get_number():
+    return Response(str(utils.num_gen()))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
---
+++ utils.py
@@ -0,0 +1,4 @@
+import random
+def num_gen():
+    x = random.randint(0,10000000)
+    return x
